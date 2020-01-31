7eb35d7fcbb0f018fbaf59303572403e8b194c0d
DL <daniil.labuz@gmail.com>
2020-01-30 17:56:22 -0800

    mb

--- examtest/2last_word/last_word.c
+++ examtest/2last_word/last_word.c
@@ -5,16 +5,16 @@ int	main(int ac, char **av)
 	if (ac == 2)
 	{
 		while (*av[1])
-		av[1]++;
+			av[1]++;
 		while (av[1] >= 0 && (*av[1] == ' ' || *av[1] == '\t' || *av[1] == '\0'))
-		av[1]--;
+			av[1]--;
 		while (av[1] >= 0 && *av[1] != ' ' && *av[1] != '\t')
-		av[1]--;
+			av[1]--;
 		av[1]++;
-		while (*av[1] != '\0' && *av[1] != ' ' && *av[1] != '\t')
+		while (*av[1] && *av[1] != ' ' && *av[1] != '\t')
 			{
-				write(1, av[1]++, 1);
-				av[1]++;
+				write(1, av[1], 1);
+				++av[1];
 			}
 	}
 	write(1, "\n", 1);
